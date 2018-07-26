b12f22a0bd66ce381707dbe4039d905f207f8924
saar.grin <saar.grin@sisense.com>
2018-07-26 10:50:02 +0300

    test

---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+result = sock.connect_ex(('127.0.0.1',8181))
+if result == 0:
+   print "Port is open"
+else:
+   print "Port is not open"
